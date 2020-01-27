559c67fe0cffe3ade9c762ab9d35c3920800f441
spacemanidol <dacamp@microsoft.com>
2020-01-27 05:14:38 -0800

    Update prep.sh

--- code/prep.sh
+++ code/prep.sh
@@ -1,10 +1,5 @@
 cd data
 wget https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-103-v1.zip
-wget https://storage.googleapis.com/albert_models/albert_base_v1.tar.gz
-tar -xzvf albert_base_v1.tar.gz
-rm albert_base_v1.tar.gz
-#wget https://storage.googleapis.com/albert_models/albert_large_v1.tar.gz
-#wget https://storage.googleapis.com/albert_models/albert_xlarge_v1.tar.gz
 unzip wikitext-103-v1.zip
 cat wikitext-103/wiki.train.tokens wikitext-103/wiki.valid.tokens wikitext-103/wiki.test.tokens > wikitext.tokens
 rm -rf wikitext-103
